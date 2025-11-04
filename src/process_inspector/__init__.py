605edd6dfdc813f60b26587533856ff6608f55f3
Tim Santor <tsantor@xstudios.com>
2025-11-04 18:24:30 -0500

    import ScheduledTask

--- src/process_inspector/__init__.py
+++ src/process_inspector/__init__.py
@@ -1,12 +1,21 @@
 import logging
 
-from .appcontrol import NativeApp  # noqa: F401
-from .oscontrol import OperatingSystem  # noqa: F401
-from .servicecontrol import Service  # noqa: F401
-from .teamviewer import Teamviewer  # noqa: F401
+from .appcontrol import NativeApp
+from .oscontrol import OperatingSystem
+from .servicecontrol import Service
+from .taskcontrol import ScheduledTask
+from .teamviewer import Teamviewer
+
+__version__ = "0.1.8"
+
+__all__ = [
+    "NativeApp",
+    "OperatingSystem",
+    "ScheduledTask",
+    "Service",
+    "Teamviewer",
+]
 
 # Basic logger setup; users of this package can configure logging as needed
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
-
-__version__ = "0.1.8"
