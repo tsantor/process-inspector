e327563012dee1e40709c9a2fb580e78e88fa941
Tim Santor <tsantor@xstudios.com>
2025-12-03 17:09:12 -0500

    add more test coverage

--- src/process_inspector/__init__.py
+++ src/process_inspector/__init__.py
@@ -7,7 +7,7 @@ from .servicecontrol import Service
 from .taskcontrol import ScheduledTask
 from .teamviewer import Teamviewer
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "NativeApp",
