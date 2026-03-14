5a7c650944421df9455cc2739fa5397d4acfb8ec
Tim Santor <tsantor@xstudios.com>
2026-03-14 00:21:55 -0400

    refactor: centralize teamviewer platform wiring in infrastructure factory

--- src/process_inspector/teamviewer/infrastructure/factory.py
+++ src/process_inspector/teamviewer/infrastructure/factory.py
@@ -1,9 +1,31 @@
 from __future__ import annotations
 
+import sys
+
+from process_inspector.servicecontrol.implementations import SystemCtl
 from process_inspector.teamviewer.application.service import TeamviewerService
-from process_inspector.teamviewer.infrastructure.repository import (
-    build_teamviewer_controller,
-)
+
+if sys.platform == "darwin":
+    from process_inspector.appcontrol.mac import App as NativeApp
+    from process_inspector.teamviewer.mac import get_teamviewer_path
+elif sys.platform == "win32":
+    from process_inspector.appcontrol.windows import App as NativeApp
+    from process_inspector.teamviewer.windows import get_teamviewer_path
+else:
+    from process_inspector.appcontrol.linux import App as NativeApp
+    from process_inspector.teamviewer.linux import get_teamviewer_path
+
+
+def build_teamviewer_controller(*, state_change_callback=None):
+    if sys.platform == "linux":
+        return SystemCtl(
+            "teamviewerd.service",
+            state_change_callback=state_change_callback,
+        )
+    return NativeApp(
+        get_teamviewer_path(),
+        state_change_callback=state_change_callback,
+    )
 
 
 def build_teamviewer_service(*, state_change_callback=None) -> TeamviewerService:
