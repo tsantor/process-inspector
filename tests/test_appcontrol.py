d669a617cea06118cf4b72bc35365ac3f84be4b1
Tim Santor <tsantor@xstudios.com>
2026-06-01 12:11:08 -0400

    fix test

--- tests/test_appcontrol.py
+++ tests/test_appcontrol.py
@@ -7,8 +7,8 @@ from unittest.mock import patch
 import psutil
 import pytest
 
-from process_inspector.appcontrol import AppBase
 from process_inspector.appcontrol import NativeApp
+from process_inspector.appcontrol.infrastructure.base import AppBase
 
 from .utils import wait_for_condition
 
